9ff74210ea2046662d475af46dbb753b535451f4
Ali Seifi <seifiali84@gmail.com>
2023-09-06 20:47:59 +0330

    testing slash commands

--- Norex_Discord_Player_Bot/Bot.cs
+++ Norex_Discord_Player_Bot/Bot.cs
@@ -1,6 +1,6 @@
 ﻿using DSharpPlus;
-using DSharpPlus.EventArgs;
-using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
 using DSharpPlus.VoiceNext;
 
 using System;
@@ -9,11 +9,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Norex_Discord_Player_Bot
 {
 
-    public class Bot
+    public class SlashCommands : ApplicationCommandModule
     {
+        [SlashCommand("test", "A slash command made to test the DSharpPlus Slash Commands extension!")]
+        public async Task TestCommand(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Success!"));
+        }
+        [SlashCommand("delaytest", "A slash command made to test the DSharpPlus Slash Commands extension!")]
+        public async Task DelayTestCommand(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
 
+            //Some time consuming task like a database call or a complex operation
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Thanks for waiting!"));
+        }
+        //commands
     }
 }
--- Norex_Discord_Player_Bot/Program.cs
+++ Norex_Discord_Player_Bot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DSharpPlus;
+using DSharpPlus.SlashCommands;
 using System;
 using System.Threading.Tasks;
 
@@ -16,8 +17,23 @@ namespace Norex_Discord_Player_Bot
                 Intents = DiscordIntents.AllUnprivileged
             });
 
+            // just available in PV Chat 
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                {
+                    await e.Message.RespondAsync("pong!");
+                    Console.WriteLine(e.Message.Content);
+                }
+                Console.WriteLine(e.Message.Content.Length);
+                Console.WriteLine(e.MentionedUsers.First().Username);
+            };
+            var slash = discord.UseSlashCommands();
+            slash.RegisterCommands<SlashCommands>();
+
             await discord.ConnectAsync();
-            Task.Delay(-1);
+
+            await Task.Delay(-1);
         }
     }
 }
