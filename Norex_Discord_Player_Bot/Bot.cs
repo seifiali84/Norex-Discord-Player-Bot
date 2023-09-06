b28c4b71eb99e3c3eaa7823eb561aa6b3070f761
Ali Seifi <seifiali84@gmail.com>
2023-09-06 16:25:49 +0330

    first commit and add gitignore file

---
+++ Norex_Discord_Player_Bot/Bot.cs
@@ -0,0 +1,19 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.VoiceNext;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Norex_Discord_Player_Bot
+{
+
+    public class Bot
+    {
+
+    }
+}
---
+++ Norex_Discord_Player_Bot/Program.cs
@@ -0,0 +1,23 @@
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+
+
+namespace Norex_Discord_Player_Bot
+{
+    public class Program
+    {
+        public static async Task Main(string[] Args)
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = File.ReadAllText("token.txt"),
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            await discord.ConnectAsync();
+            Task.Delay(-1);
+        }
+    }
+}
